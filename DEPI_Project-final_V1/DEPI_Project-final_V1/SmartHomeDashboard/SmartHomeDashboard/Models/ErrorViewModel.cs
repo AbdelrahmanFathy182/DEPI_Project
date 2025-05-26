a4a812ac922348553e1648a7720de6ae143674eb
abdelrahman <abdelrahman.fathy.helal@gmail.com>
2025-05-26 23:39:39 +0300

    no

--- DEPI_Project-final_V1/DEPI_Project-final_V1/SmartHomeDashboard/SmartHomeDashboard/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartHomeDashboard.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
